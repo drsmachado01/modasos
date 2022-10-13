bdecf21cd9a5474e068a549240406afc62f54be4
Darlan Machado <dr.bsi@hotmail.com>
2022-10-13 18:21:58 -0300

    Ajustes

--- start.js
+++ start.js
@@ -7,8 +7,11 @@ const port = process.env.PORT || 80;
 
 // sendFile will go here
 app.get('/', function(req, res) {
+  console.log('Ok, passando por aqui e redirecionando para home');
   res.sendFile(path.join(__dirname, '/home.html'));
 });
 
-app.listen(port);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 console.log('Server started at http://localhost:' + port);
